fbbf16184e31f4afd759fdca9a11d908564976cb
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-09 19:12:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ car_collection = [
 
 while True:  
   action = input("Enter action (add, remove, new, check, find, read, or end): ")
-  
+
   if action == "end":
     break  
-  
+
   elif action == "add":
     car_name = input("Enter your car's name: ")
     car_info = input("Enter key info of the car: ")
@@ -17,8 +17,15 @@ while True:
       car_collection[slot_number][0] = car_name
       car_collection[slot_number][1] = car_info
       print(car_collection)
+      x = str(car_collection) + '\n'  # Append new line character
+      with open("car.txt" , "a") as file:
+        file.write(x)
     else:
       print("Slot unavailable")
+      x = '\n' + str(car_collection) + '\n'  # Append new line character
+      with open("car.txt" , "a") as file:
+        file.write(x)
+
 
   elif action == "remove":
     slot = int(input("Enter slot number you would like to empty: "))
@@ -26,6 +33,9 @@ while True:
       car_collection[slot][0] = ""
       car_collection[slot][1] = ""
       print(car_collection)
+      x = '\n' + str(car_collection) + '\n'  # Append new line character
+      with open("car.txt" , "a") as file:
+        file.write(x)
     else:
       print("Slot is already empty")
 
@@ -35,17 +45,25 @@ while True:
       car_collection.append(["", ""])
       row = len(car_collection)
     print(f"You currently have {row} in database")
-    print(car_collection)
+    x = '\n' + str(car_collection) + '\n'  # Append new line character
+    with open("car.txt" , "a") as file:
+      file.write(x)
 
   elif action == "check":
     c = int(input("Enter slot number you would like to check: "))
     print(car_collection[c])
+    x = '\n' + str(car_collection) + '\n'  # Append new line character
+    with open("car.txt" , "a") as file:
+      file.write(x)
 
   elif action == "find":
     z = input("Which words you looking for in data base")
     for i in range(len(car_collection)):
       if z in car_collection[i]:
         print("item found")
+        x = '\n' + str(car_collection) + '\n'  # Append new line character
+        with open("car.txt" , "a") as file:
+          file.write(x)
 
   elif action == "read":
     z = input("Which word are you looking for in the database: ")
@@ -54,5 +72,8 @@ while True:
       if z in car_collection[i]:
         print(f"Item found at index {i}, key info: {car_collection[i][1]}")
         found = True
+        x = '\n' + str(car_collection) + '\n'  # Append new line character
+        with open("car.txt" , "a") as file:
+          file.write(x)
     if not found:
       print("Item not found")
